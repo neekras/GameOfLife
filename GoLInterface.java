0a5a30f2bfc9c1cda7b334db80e00fc0640d76a0
neekras <nick.hampshire93@gmail.com>
2014-06-13 20:29:44 +1000

    Create GoLInterface.java

---
+++ GoLInterface.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class GoLInterface
+{
+	//long start = System.nanoTime(); //for timing program
+	GoL gol = new GoL();
+	//Scanner scanner = new Scanner(System.in);
+	public void run()
+	{
+		gol.run();
+		//double elapsedTimeInSec = (System.nanoTime() - start) * 1.0e-9; //for timing program (when not infinitely looping)
+		//System.out.println(elapsedTimeInSec);
+	}
+	
+	/**
+	 * Creates instance of GoLInterface, use run() for non-static method
+	 */
+	public static void main(String[] args)
+	{
+		GoLInterface ifc = new GoLInterface();
+		ifc.run();
+	}
+}
